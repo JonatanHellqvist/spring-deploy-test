c40cb753fceeb6beca3c6a3efe0bc5ce6938073f
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-04-22 10:59:45 +0200

    test for deploy

---
+++ src/test/java/com/springdeploytest/springdeploytest/SpringDeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdeploytest.springdeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
